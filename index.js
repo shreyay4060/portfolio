cf63382105c9c34ba8eb9082b60051e6ec0a5516
shreyay4060 <shreyay4060@gmail.com>
2025-01-07 22:10:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+    
+        var typed = new Typed('#element', {
+          strings: ['Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer','Web-Developer','Web-Designer', 'Frontend Developer','Graphic Designer','UI/UX Designer'],
+          typeSpeed: 70,
+        });
+      
+
+  // document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  //   anchor.addEventListener("click", function (e) {
+  //     e.preventDefault();
+  //     document.querySelector(this.getAttribute("href")).scrollIntoView({
+  //       behavior: "smooth",
+  //     });
+  //   });
+  // });
+  
+  
+  // const copyrightLink = document.querySelector(".copyright a");
+  // if (copyrightLink) {
+  //   copyrightLink.style.textDecoration = "none";
+  //   copyrightLink.style.color = "rgb(237, 69, 97)";
+  // }
+  
+
+
+  
+
+  // const socialIcons = document.querySelectorAll(".social-icons a");
+  // socialIcons.forEach((icon) => {
+  //   icon.addEventListener("mouseenter", () => {
+  //     icon.style.transform = "scale(1.2)";
+  //     icon.style.transition = "transform 0.3s ease";
+  //   });
+  //   icon.addEventListener("mouseleave", () => {
+  //     icon.style.transform = "scale(1)";
+  //   });
+  // });
+  
+  // //copyright
+  // const copyrightYear = document.querySelector(".copyright .fa-copyright");
+  // if (copyrightYear) {
+  //   const currentYear = new Date().getFullYear();
+  //   copyrightYear.insertAdjacentText("afterend", ` ${currentYear}`);
+  // }
